c3e09902e563f71744c07a085eb6672776b72c69
nicolaspatch <68646099+nicolaspatch@users.noreply.github.com>
2021-06-19 20:14:16 +0200

    change to test reverting to prev commit

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -4,5 +4,6 @@ class App {
     public static void main(String[] args) {
         System.out.println("Hello ...!!!");
         // this is my comment from PCDESK
+        // this is a change to test reverting to previous commit
     }
 }
