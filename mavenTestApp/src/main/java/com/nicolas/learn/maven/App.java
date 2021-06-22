f1892e4138b1ca2e46af56ddb8b848480e16af77
RAZE Nicolas <nicopatch.nicolas@gmail.com>
2021-06-22 16:36:36 +0200

    created maven projects

---
+++ mavenTestApp/src/main/java/com/nicolas/learn/maven/App.java
@@ -0,0 +1,13 @@
+package com.nicolas.learn.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
